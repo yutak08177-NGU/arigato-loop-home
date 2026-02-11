ca116f20a7b9a34f51a366c9fd01423b86d38fbe
yutak08177-NGU <yutak08177@gmail.com>
2026-02-11 14:08:37 +0900

    ホスティング

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
 };
 
 export default nextConfig;
